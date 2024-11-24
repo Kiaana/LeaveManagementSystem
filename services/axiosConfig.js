27b6b66006039c3ad3d504d29329b337f3119832
王思远 <kiaana@163.com>
2024-11-25 02:40:28 +0800

    在 Axios 配置中添加 withCredentials 选项，以确保请求携带 Cookie

--- services/axiosConfig.js
+++ services/axiosConfig.js
@@ -6,6 +6,7 @@ const axiosInstance = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
+  withCredentials: true, // 确保 Axios 请求携带 Cookie
 });
 
 export default axiosInstance;
